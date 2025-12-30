be76a2721ad7e57a784f9586f506342d60cbc811
Strahil731 <hristovstrahil5@gmail.com>
2025-12-30 09:30:05 +0200

    Create Най-голямо число.js

---
+++ Най-голямо число.js
@@ -0,0 +1,34 @@
+function biggestNumber(input) {
+    let bigNumber = Number(input.shift());
+
+    let i = 0;
+    while (input[i] !== "Stop") {
+        let currentNumber = Number(input[i]);
+
+        if (currentNumber > bigNumber) {
+            bigNumber = currentNumber;
+        }
+
+        i++;
+    }
+
+    console.log(bigNumber);
+}
+
+biggestNumber(
+    [
+        "45",
+        "-20",
+        "7",
+        "99",
+        "Stop"
+    ]
+)
+
+biggestNumber(
+    [
+        "-1",
+        "-2",
+        "Stop"
+    ]
+)
